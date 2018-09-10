dc1af72484ccfb64b247e71ed7486a6fc271ffb1
Charles D Hamilton <lespaul36@gmail.com>
2018-09-09 23:18:26 -0700

    Creating PlugIn

---
+++ src/main/java/com/github/lespaul361/javadocplugin/JavaDocFillIn.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.github.lespaul361.javadocplugin;
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.MojoFailureException;
+import org.apache.maven.plugins.annotations.Mojo;
+/**
+ *
+ * @author David Hamilton
+ */
+@Mojo(name="Javadoc Fill In")
+public class JavaDocFillIn extends AbstractMojo{
+
+    @Override
+    public void execute() throws MojoExecutionException, MojoFailureException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+
+}
