4b186e54854b5d0635eeb4325c9dd86348cdbd3b
Charles D Hamilton <lespaul36@gmail.com>
2018-09-10 22:46:38 -0700

    some tweaks to the plug in

---
+++ src/main/java/com/github/lespaul361/javadocplugin/Filler.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.github.lespaul361.javadocplugin;
+
+import org.apache.maven.plugins.annotations.Parameter;
+
+/**
+ *
+ * @author David Hamilton
+ */
+public class Filler {
+
+    @Parameter
+    String variable;
+    @Parameter
+    String text;
+    
+}
